c6fd6e03f3c82be8ec5df5da16a359be546aa3c2
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2025-07-12 05:37:11 +0300

    refactor some names

--- composeApp/src/commonMain/kotlin/org/batuhanerdem/todoappcmp/model/root/RootComponent.kt
+++ composeApp/src/commonMain/kotlin/org/batuhanerdem/todoappcmp/model/root/RootComponent.kt
@@ -1,11 +1,11 @@
 package org.batuhanerdem.todoappcmp.model.root
 
-import org.batuhanerdem.todoappcmp.ui.home.DefaultHomeComponent
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 import org.batuhanerdem.todoappcmp.navigation.Config
+import org.batuhanerdem.todoappcmp.ui.home.HomeComponent
 import org.batuhanerdem.todoappcmp.ui.home.add.AddComponent
-import org.batuhanerdem.todoappcmp.ui.settings.DefaultSettingsComponent
+import org.batuhanerdem.todoappcmp.ui.settings.SettingsComponent
 
 interface RootComponent {
     val stack: Value<ChildStack<*, Child>>
@@ -13,8 +13,8 @@ interface RootComponent {
     fun onTabSelected(config: Config)
 
     sealed interface Child {
-        class HomeChild(val component: DefaultHomeComponent) : Child
-        class SettingsChild(val component: DefaultSettingsComponent) : Child
+        class HomeChild(val component: HomeComponent) : Child
+        class SettingsChild(val component: SettingsComponent) : Child
         class AddChild(val component: AddComponent) : Child
     }
 }
--- composeApp/src/commonMain/kotlin/org/batuhanerdem/todoappcmp/ui/settings/SettingsContent.kt
+++ composeApp/src/commonMain/kotlin/org/batuhanerdem/todoappcmp/ui/settings/SettingsContent.kt
@@ -1,20 +1,21 @@
 package org.batuhanerdem.todoappcmp.ui.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun SettingsContent(component: SettingsComponent) {
-    Column(Modifier.fillMaxSize().padding(30.dp)) {
-
-        Text("setme", fontSize = 30.sp)
-
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("github.com/batuhanerdem")
     }
 }
